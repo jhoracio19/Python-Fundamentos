b3e109f6c4756a876da4989581d535cb7ad22966
Jose Horacio <jhoracio19@hotmail.com>
2025-10-02 22:13:55 -0600

    agregando ejercicio de cajero automatico

--- 00-mini_proyects/03-bank.py
+++ 00-mini_proyects/03-bank.py
@@ -43,3 +43,28 @@ cuenta1.transferir(200, cuenta2)
 cuenta1.info()
 print("---------------------------------------------------")
 cuenta2.info()
+
+cuenta = CuentaBancaria("Horacio", 1000)
+
+while True:
+    print("\n--- Cajero Automático ---")
+    print("1. Consultar saldo")
+    print("2. Depositar")
+    print("3. Retirar")
+    print("4. Salir")
+
+    opcion = input("Elige una opción: ")
+
+    if opcion == "1":
+        cuenta.info()
+    elif opcion == "2":
+        monto = float(input("Monto a depositar: "))
+        cuenta.depositar(monto)
+    elif opcion == "3":
+        monto = float(input("Monto a retirar: "))
+        cuenta.retirar(monto)
+    elif opcion == "4":
+        print("Gracias por usar el cajero. ¡Adiós!")
+        break
+    else:
+        print("Opción no válida, intenta de nuevo.")
